cf2fd1e2eb63c2061a04172f56752c760a98788c
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-05-03 21:49:39 +0200

    agregado Cliente SOAP MTIS Prac3 Ex1

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0cdc697a0cde97f4ffe75053766a18469670f13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813d1dffd3dfad7bedb6181dc6f39f5aff87a030")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
